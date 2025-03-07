057a4e5bb0bb7fb9833bee37951b48f74f5f2a10
ceelestin <celest1.eve@gmail.com>
2025-03-07 16:46:43 +0100

    add different config

---
+++ thresholds.py
@@ -0,0 +1,48 @@
+# %%
+import numpy as np
+np.logspace(1,4,num=13)
+
+# %%
+np.logspace(3,np.log(1778)/np.log(10),num=6)
+np.logspace(np.log(1778)/np.log(10),np.log(3162)/np.log(10),num=4)
+
+# %%
+print(np.logspace(np.log(32)/np.log(10),np.log(56)/np.log(10),num=6))
+print(np.logspace(np.log(56)/np.log(10),np.log(100)/np.log(10),num=4))
+
+# %%
+print(np.logspace(np.log(56)/np.log(10),np.log(100)/np.log(10),num=6))
+print(np.logspace(np.log(100)/np.log(10),np.log(178)/np.log(10),num=4))
+
+# %%
+print(np.logspace(np.log(100)/np.log(10),np.log(178)/np.log(10),num=6))
+print(np.logspace(np.log(178)/np.log(10),np.log(316)/np.log(10),num=4))
+
+# %%
+print(np.logspace(np.log(178)/np.log(10),np.log(316)/np.log(10),num=6))
+print(np.logspace(np.log(316)/np.log(10),np.log(562)/np.log(10),num=4))
+
+# %%
+print(np.logspace(np.log(316)/np.log(10),np.log(562)/np.log(10),num=6))
+print(np.logspace(np.log(562)/np.log(10),np.log(1000)/np.log(10),num=4))
+
+# %%
+print(np.logspace(np.log(562)/np.log(10),np.log(1000)/np.log(10),num=6))
+print(np.logspace(np.log(1000)/np.log(10),np.log(1778)/np.log(10),num=4))
+
+# %%
+print(np.logspace(np.log(1000)/np.log(10),np.log(1778)/np.log(10),num=6))
+print(np.logspace(np.log(1778)/np.log(10),np.log(3162)/np.log(10),num=4))
+
+# %%
+print(np.logspace(np.log(1778)/np.log(10),np.log(3162)/np.log(10),num=6))
+print(np.logspace(np.log(3162)/np.log(10),np.log(5623)/np.log(10),num=4))
+
+# %%
+print(np.logspace(np.log(3162)/np.log(10),np.log(5623)/np.log(10),num=6))
+print(np.logspace(np.log(5623)/np.log(10),np.log(10000)/np.log(10),num=4))
+
+# %%
+print(np.logspace(np.log(5623)/np.log(10),np.log(10000)/np.log(10),num=6))
+
+# %%
